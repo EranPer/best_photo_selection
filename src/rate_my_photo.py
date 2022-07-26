2c4d8c98a0f70ceecbcfdb9b0fb552aa18f874c0
Eran Perelman <73245710+EranPer@users.noreply.github.com>
2022-07-26 14:13:24 +0300

    Update rate_my_photo.py

--- src/rate_my_photo.py
+++ src/rate_my_photo.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     types_of_birds = sorted(list(all_image_files['test'].keys()))
     types_of_birds = [bird.title() for bird in types_of_birds]
 
-    st.title('Welcome To Project Eagle Vision!')
+    st.title('Rate My Photo!')
     instructions = """
         Either upload your own image or select from
         the sidebar to get a preconfigured image.
