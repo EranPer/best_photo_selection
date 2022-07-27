40011a1cdd89ba2d0d37b6e1395b11b5018bd80d
Eran Perelman <73245710+EranPer@users.noreply.github.com>
2022-07-27 14:02:47 +0300

    Update rate_my_photo.py

--- src/rate_my_photo.py
+++ src/rate_my_photo.py
@@ -135,5 +135,7 @@ if __name__ == '__main__':
         df.iloc[idx, 0] = p[0]
         df.iloc[idx, 1] = str(p[1]) + '%'
     st.write(df.to_html(escape=False), unsafe_allow_html=True)
-    
-    st.write('\nThie project was developed by Eran Perelman, Asi Sheratzki and Ary Korenvais with the guidance of Morris Alper.')
+    credits = """
+        This project was developed by Eran Perelman, Asi Sheratzki and Ary Korenvais with the guidance of Morris Alper.
+        """
+    st.write(credits)
